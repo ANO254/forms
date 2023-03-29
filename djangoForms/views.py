fe1b76ed9904c38bb4142a6a22878dbb1b3f6cae
ANO254 <tailoonarnold@gmail.com>
2023-03-29 17:01:54 +0300

    Initial commit

---
+++ djangoForms/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .forms import Contactform
+
+def contact(request):
+    if request =='POST':
+        form=Contactform(request.POST)
+        if form.is_valid():
+            return render(request,'')
+    else:
+        form=Contactform()
+    return render(request,'contact.html',{'form':form})
